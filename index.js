8fbdf2533e116e395f1c51b915d9884fc8285589
achencompass360 <abel.chen@compass360tech.com>
2021-01-07 22:53:43 -0500

    reverted static page

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ const grocery = require("./routes/groceryRoute");
 app.use("/groceries", grocery);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'front_end', 'build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
-  });
+	app.use(express.static('client/build'));
 }
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 5000);
 
