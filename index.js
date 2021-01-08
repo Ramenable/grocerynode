fcb38530da450be14724150944999ad995e7a691
achencompass360 <abel.chen@compass360tech.com>
2021-01-07 22:44:53 -0500

    removed stuff

--- index.js
+++ index.js
@@ -24,9 +24,5 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
 app.listen(process.env.PORT || 5000);
 
